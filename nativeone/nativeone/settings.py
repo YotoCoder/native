6e69de33f0196e1a008dbda598d1b49242594a33
Yonathan <yotoelectronics@gmail.com>
2021-09-05 01:13:16 -0500

    estable 1 con statics

--- nativeone/nativeone/settings.py
+++ nativeone/nativeone/settings.py
@@ -121,7 +121,15 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,'static'),
+)
+
+print(STATICFILES_DIRS)
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
